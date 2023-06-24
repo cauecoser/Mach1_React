95ff5d76e66c54a775fa116360b1cccfeee208b4
Cauê Coser <cauecoser@gmail.com>
2023-06-24 17:47:48 -0300

    Solução do exercício prático da aula 010.

--- 010-redoucers/src/main.jsx
+++ 010-redoucers/src/main.jsx
@@ -5,9 +5,7 @@ import './index.css'
 import { ProductProvider } from './ProductContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ProductProvider>
-      <App />
-    </ProductProvider>
-  </React.StrictMode>,
+  <ProductProvider>
+    <App />
+  </ProductProvider>
 )
